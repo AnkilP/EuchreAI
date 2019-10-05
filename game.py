8362f4767e85041ff5cb817f20daae2f7d81a35a
AnkilP <a293pate@uwaterloo.ca>
2019-10-05 11:25:28 -0700

    merge commit

--- game.py
+++ game.py
@@ -5,9 +5,10 @@ class Game:
 
     def __init__(self, number_of_players):
         self.number_of_players = number_of_players
-        self.suits = []
-
+        
     def decide_trump(self):
         while True:
             for suit in self.suits:
                 yield suit
+
+    
